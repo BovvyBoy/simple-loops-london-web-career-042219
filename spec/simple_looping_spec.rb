c78ed721ac7a6e3194124f4438aa9deb0dcc293a
Daniel Bovington <d.bovington@gmail.com>
2019-03-17 15:16:15 +0000

    Automatically backed up by Learn

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -1,14 +1,13 @@
 require_relative './spec_helper'
 
 def loop
- 1.	counter = 0
-2.	 
-3.	loop do 
-counter += 1
-puts "Iteration #{counter} of the loop"
-if counter >= 10 
-break
-end
+counter = 0
+  loop do 
+  counter += 1
+  puts "Iteration #{counter} of the loop"
+  if counter >= 10 
+  break
+  end
 end
 
 end
